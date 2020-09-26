c20cf9bc137fbc53541fcf46d64620eb5c680677
Annehelen-ltda <71146646+Annehelen-ltda@users.noreply.github.com>
2020-09-26 18:19:13 -0300

    Java 6 Pass

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -15,9 +15,75 @@ public class CupomFiscal {
 	public static String CNPJ = "42.591.651/0797-34";
 	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
 
+	private static final String ENDLN = System.lineSeparator();
+
+	private static boolean isEmpty(String str){
+        int spaceCount = str.length() - str.replace(".", "").length();
+        return (str == null || spaceCount == str.length());
+    }
+	
 	public static String dadosLoja() {
 		//Implemente aqui
-		return null;
+		String num = NUMERO + "";
+		if(NOME_LOJA.equals("")){
+			throw new RuntimeException("O campo nome da loja é obrigatório");
+		}
+		if(LOGRADOURO.equals("")){
+			throw new RuntimeException("O campo logradouro do endereço é obrigatório");
+		}
+		if(NUMERO == 0){
+			num = "s/n";
+		}
+		if(MUNICIPIO.equals("")){
+			throw new RuntimeException("O campo município do endereço é obrigatório");
+		}
+		if(ESTADO.equals("")){
+			throw new RuntimeException("O campo estado do endereço é obrigatório");
+		}
+		if(CNPJ.equals("")){
+			throw new RuntimeException("O campo CNPJ da loja é obrigatório");
+		}
+		if(INSCRICAO_ESTADUAL.equals("")){
+			throw new RuntimeException("O campo inscrição estadual da loja é obrigatório");
+		}
+		var part2 = LOGRADOURO + ", " + num;
+		if (! isEmpty (COMPLEMENTO)){
+			part2 += " " + COMPLEMENTO;
+			}
+		var part3 = "";
+		if (! isEmpty (BAIRRO)){
+			part3 += BAIRRO + " - ";
+		}
+		part3 += MUNICIPIO + " - " + ESTADO;
+		
+		var part4 = "";
+		if (! isEmpty (CEP)){
+			part4 = "CEP:" + CEP;
+			}
+		if (! isEmpty (TELEFONE)){
+			if (! isEmpty (part4)){
+				part4 += " ";
+			}
+			part4 += "Tel " + TELEFONE;
+			}
+		if (! isEmpty(part4)){
+			part4 += ENDLN;
+			}
+		var part5 = "";
+		if (! isEmpty (OBSERVACAO)){
+			part5 += OBSERVACAO;
+			}
+
+		String output = NOME_LOJA + ENDLN;
+        output += part2 + ENDLN;
+        output += part3 + ENDLN;
+        output += part4;
+        output += part5 + ENDLN;
+        output += "CNPJ: " + CNPJ + ENDLN;
+		output += "IE: " + INSCRICAO_ESTADUAL+ENDLN;
+		
+
+		return output;
 	}
 
 }
--- src/test/java/com/example/project/TestCupomFiscal.java
+++ src/test/java/com/example/project/TestCupomFiscal.java
@@ -66,7 +66,7 @@ public class TestCupomFiscal {
 			"IE: 123456789" + BREAK;
 
 	@BeforeAll
-	public void setup() {
+	public static void setup() {
 		CupomFiscal.NOME_LOJA = "Loja 1";
 		CupomFiscal.LOGRADOURO = "Log 1";
 		CupomFiscal.NUMERO = 10;
@@ -90,21 +90,21 @@ public class TestCupomFiscal {
 	public void nomeVazio() {
 		CupomFiscal.NOME_LOJA = "";
 		verificarCampoObrigatorio("O campo nome da loja é obrigatório");
-		CupomFiscal.NOME_LOJA = "Arcos Dourados Com. de Alimentos LTDA";
+		CupomFiscal.NOME_LOJA = "Loja 1";
 	}
 
 	@Test
 	public void logradouroVazio() {
 		CupomFiscal.LOGRADOURO = "";
 		verificarCampoObrigatorio("O campo logradouro do endereço é obrigatório");
-		CupomFiscal.LOGRADOURO = "Av. Projetada Leste";
+		CupomFiscal.LOGRADOURO = "Log 1";
 	}
 
 	@Test
 	public void numeroZero() {
 		CupomFiscal.NUMERO = 0;
 		rodarTestarRetorno(TEXTO_ESPERADO_SEM_NUMERO);
-		CupomFiscal.NUMERO = 500;
+		CupomFiscal.NUMERO = 10;
 	}
 
 	@Test
@@ -125,14 +125,14 @@ public class TestCupomFiscal {
 	public void municipioVazio() {
 		CupomFiscal.MUNICIPIO = "";
 		verificarCampoObrigatorio("O campo município do endereço é obrigatório");
-		CupomFiscal.MUNICIPIO = "Campinas";
+		CupomFiscal.MUNICIPIO = "Mun 1";
 	}
 
 	@Test
 	public void estadoVazio() {
 		CupomFiscal.ESTADO = "";
 		verificarCampoObrigatorio("O campo estado do endereço é obrigatório");
-	    CupomFiscal.ESTADO = "SP";
+	    CupomFiscal.ESTADO = "E1";
 	}
 
 	@Test
@@ -160,34 +160,42 @@ public class TestCupomFiscal {
 	public void cnpjVazio() {
 		CupomFiscal.CNPJ = "";
 		verificarCampoObrigatorio("O campo CNPJ da loja é obrigatório");
-	    CupomFiscal.CNPJ = "42.591.651/0797-34";
+	    CupomFiscal.CNPJ = "11.111.111/1111-11";
 	}
 
 	@Test
 	public void inscricaoEstadualVazia() {
 		CupomFiscal.INSCRICAO_ESTADUAL = "";
 		verificarCampoObrigatorio("O campo inscrição estadual da loja é obrigatório");
-		CupomFiscal.INSCRICAO_ESTADUAL = "244.898.500.113";
+		CupomFiscal.INSCRICAO_ESTADUAL = "123456789";
 	}
 	
 	@Test
 	public void exercicio02_Customizado() {
 		//Defina seus próprios valores para as variáveis a seguir 
-		CupomFiscal.NOME_LOJA = "";
-		CupomFiscal.LOGRADOURO = "";
-		CupomFiscal.NUMERO = 0;
-		CupomFiscal.COMPLEMENTO = "";
-		CupomFiscal.BAIRRO = "";
-		CupomFiscal.MUNICIPIO = "";
-		CupomFiscal.ESTADO = "";
-		CupomFiscal.CEP = "";
-		CupomFiscal.TELEFONE = "";
-		CupomFiscal.OBSERVACAO = "";
-		CupomFiscal.CNPJ = "";
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
+		CupomFiscal.NOME_LOJA = "Smelly Cat";
+		CupomFiscal.LOGRADOURO = "Rua Etheria";
+		CupomFiscal.NUMERO = 205;
+		CupomFiscal.COMPLEMENTO = "Perto da velhinha que mora em uma caverna";
+		CupomFiscal.BAIRRO = "Br. Templo do Cristal";
+		CupomFiscal.MUNICIPIO = "Beach City";
+		CupomFiscal.ESTADO = "BC";
+		CupomFiscal.CEP = "78051-604";
+		CupomFiscal.TELEFONE = "(66)4002-8922";
+		CupomFiscal.OBSERVACAO = "Por Favor ignorar os exército Intergalácticos em guerra tentando dominar o planeta";
+		CupomFiscal.CNPJ = "53.409.609/0001-85";
+		CupomFiscal.INSCRICAO_ESTADUAL = "512.670.302.653";
+
+		String expected = "Smelly Cat"+ BREAK;
+		expected += "Rua Etheria, 205 Perto da velhinha que mora em uma caverna" + BREAK;
+		expected += "Br. Templo do Cristal - Beach City - BC" + BREAK;
+		expected += "CEP:78051-604 Tel (66)4002-8922" + BREAK;
+		expected += "Por Favor ignorar os exército Intergalácticos em guerra tentando dominar o planeta" + BREAK;
+		expected +="CNPJ: 53.409.609/0001-85" + BREAK;
+		expected += "IE: 512.670.302.653";
 		
 		//E atualize o texto esperado abaixo
-		rodarTestarRetorno("" + BREAK);
+		rodarTestarRetorno(expected + BREAK);
 	}
 
 	private void rodarTestarRetorno(String expected) {
